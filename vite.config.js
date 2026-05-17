fda150563d3e8b520199f1b41b3159027490cdef
PIYUSH MEHTA <piyushmehtaa9@gmail.com>
2026-05-17 19:30:29 +0530

    Renamed project routing to NETANIS-JEWELOS

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/NETANI-JEWELOS/",
+  base: "/NETANIS-JEWELOS/",
 });
